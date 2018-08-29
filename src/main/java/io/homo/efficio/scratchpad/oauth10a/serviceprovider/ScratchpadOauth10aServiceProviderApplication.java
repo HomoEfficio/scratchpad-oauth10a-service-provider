fc454b082d95c9f71678e654ea09ef045fe63087
Homo Efficio <homo.efficio@gmail.com>
2018-08-29 23:06:39 +0900

    Initial Commit

---
+++ src/main/java/io/homo/efficio/scratchpad/oauth10a/serviceprovider/ScratchpadOauth10aServiceProviderApplication.java
@@ -0,0 +1,12 @@
+package io.homo.efficio.scratchpad.oauth10a.serviceprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScratchpadOauth10aServiceProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScratchpadOauth10aServiceProviderApplication.class, args);
+    }
+}
